5a823e1dd14e3da7fd74cdc904f62e60d6fed557
JimGu <gwt200407@gmail.com>
2023-12-20 00:16:02 +0800

    update audio

--- base/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Video, Image, Audio
-
-# Register your models here.
-admin.site.register(Video)
-admin.site.register(Image)
-admin.site.register(Audio)
