33f349e2abcbc0c76089f5b83186c6c6fda3288c
JimGu <gwt200407@gmail.com>
2023-12-20 00:10:25 +0800

    update audio

--- api/views.py
+++ api/views.py
@@ -1,8 +1,8 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.parsers import FormParser, MultiPartParser
-from base.models import Video, Image
-from .serializers import VideoSerializer, ImageSerializer
+from base.models import Video, Image, Audio
+from .serializers import VideoSerializer, ImageSerializer, AudioSerializer
 from rest_framework.views import APIView
 
 @api_view(['GET']) 
@@ -30,6 +30,22 @@ def getImage(request):
 class addImage(APIView):
     parser_classes = (MultiPartParser, FormParser)
     serializer_class = ImageSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+@api_view(['GET'])
+def getAudio(request):
+    audios = Audio.objects.all()
+    serializer = AudioSerializer(audios, many=True)
+    return Response(serializer.data)
+
+class addAudio(APIView):
+    parser_classes = (MultiPartParser, FormParser)
+    serializer_class = AudioSerializer
 
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
