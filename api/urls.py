a03a06acabc811ca19760df4565af11850b28663
JimGu <gwt200407@gmail.com>
2023-12-24 15:01:21 +0800

    update

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('video', views.getVideo),
     path('add-video', views.addVideo.as_view()),
     path('image', views.getImage),
+    path('image/utility', views.ImageScatterPlotView.as_view()),
     path('add-image', views.addImage.as_view()),
     path('audio', views.getAudio),
     path('add-audio', views.addAudio.as_view())
