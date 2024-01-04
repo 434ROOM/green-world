b1d5ea37586786298e08f4b48bfefcc83cb4596b
JimGu <gwt200407@gmail.com>
2024-01-04 12:24:39 +0800

    update

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -190,7 +190,7 @@ AUTH_USER_MODEL = 'base.UserData'
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=3),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
